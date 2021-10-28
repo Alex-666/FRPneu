89c717f9b16ee88ff1c8ec82378865366dca02fa
”Alex <al.alex13@gmail.com>
2021-10-28 08:06:01 +0300

    module FeedBack update and some CSS update
    product.css - popup для мобильной версии
    journal.css - Уменьшить дистанцию под хлебными крошками. (45px)

--- catalog/language/sk-sk/extension/module/feedback.php
+++ catalog/language/sk-sk/extension/module/feedback.php
@@ -7,18 +7,18 @@ $_['entry_enquiry']        = 'Enquiry:';
 $_['text_button']          = 'Send Enquiry';
 $_['text_button_callback'] = 'Callback';
 $_['text_send']            = 'Vyžádejte si hovor';
-$_['text_loading']         = 'Sending...';
+$_['text_loading']         = 'Posílám žádost...';
 
 // Email
-$_['email_subject']           = 'Request from %s';
+$_['email_subject']           = 'Prosba zavolat %s';
 $_['email_subject_callback']  = 'Callback from %s';
 
 // Error
 $_['error_name']     = 'Name must be between 3 and 25 characters!';
-$_['error_phone']    = 'Phone must be between 3 and 25 characters!';
+$_['error_phone']    = 'Telefón musí mať 3 až 25 znakov!';
 $_['error_email']    = 'E-Mail Address does not appear to be valid!';
 $_['error_enquiry']  = 'Enquiry must be between 10 and 3000 characters!';
-$_['text_success']   = 'Thank you for contacting us! In the near future you will call the manager.';
+$_['text_success']   = 'Ďakujeme, že ste nás kontaktovali! Čoskoro sa Vám ozve náš obchodný zástupca.';
 
 //Add
 
