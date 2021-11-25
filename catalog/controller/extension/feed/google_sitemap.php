806b99a0d99ede4708219e1271a9d4a88a8cf7e6
”Alex <al.alex13@gmail.com>
2021-11-25 16:58:33 +0200

    remive picture in Sitemap

--- catalog/controller/extension/feed/google_sitemap.php
+++ catalog/controller/extension/feed/google_sitemap.php
@@ -19,11 +19,11 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
                         $output .= '  <lastmod>' . date('Y-m-d\TH:i:sP', strtotime($product['date_modified'])) . '</lastmod>';
                     }
 					$output .= '  <priority>1.0</priority>';
-					$output .= '  <image:image>';
+					/*$output .= '  <image:image>';
 					$output .= '  <image:loc>' . $this->model_tool_image->resize($product['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_popup_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_popup_height')) . '</image:loc>';
 					$output .= '  <image:caption><![CDATA[' . $product['name'] . ']]></image:caption>';
 					$output .= '  <image:title><![CDATA[' . $product['name'] . ']]></image:title>';
-					$output .= '  </image:image>';
+					$output .= '  </image:image>';*/
 					$output .= '</url>';
 				}
 			}
