01d6159e22891506547784c8c0a992320e04e8ca
”Alex <al.alex13@gmail.com>
2022-05-17 11:01:49 +0300

    Header text

--- catalog/language/cs-cz/common/header.php
+++ catalog/language/cs-cz/common/header.php
@@ -13,4 +13,4 @@ $_['text_logout']                                 = 'Odhlásit se';
 $_['text_checkout']                               = 'Objednat';
 $_['text_search']                                 = 'Vyhledat';
 $_['text_all']                                    = 'Zobrazit vše';
-$_['hader_text_twelve']                           = '<p class="first">Objednávky z Turska do 12:00 můžeme doručit po Praze a v okolí s poplatkem 99,- Kč v ten samý pracovní den!</p><p class="two">97% naších pneumatik je vyrobeno v roce 2021/2022</p>';
+$_['hader_text_twelve']                           = '<p class="first">97% naších pneumatik je vyrobeno v roce 2021/2022</p><p class="two"></p>';
